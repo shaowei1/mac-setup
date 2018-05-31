55c79292518e999ca1fc88a710962181cf754fcf
jsy <jsycdut@gmail.com>
2018-06-01 04:53:45 +0800

    完成了初始化脚本，待测试

---
+++ install_app_for_mac.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+cat << -EOF
+############################################################
+# 当前脚本用于在运行OS X的电脑上安装应用程序
+# 原理为：利用homebrew作为OS X的包管理器
+#         brew install 安装命令行程序
+#         brew cask install 安装GUI程序
+#         Happy coding ~ Happy life.
+# Author: jsycdut <jsycdut@gmail.com>
+############################################################
+-EOF
+
+# 安装Homebrew
+install_homebrew(){
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+# GUI软件包清单
+# 在这里添加或者删除你需要的GUI软件包名称
+# 用于开发的jetbrains公司的产品可能版本较低(commercial projects)
+# 可以在安装以后手动升级
+
+brew_cask_app_list=(
+iina
+alfred
+google-chrome
+firefox
+wireshark
+etcher
+telegram-desktop
+intellij-idea
+pycharm
+clion
+datagrip
+webstorm
+)
+
+# 安装GUI软件包
+install_cask_app(){
+	for app in brew_cask_app_list; do
+		brew cask install $app
+	done
+}
+
+# CLI软件包清单
+brew_cli_app_list=(
+vim
+wget
+mysql
+)
+
+# 安装CLI软件包
+install_cli_app(){
+	for app in brew_cli_app_list;do
+		brew install $app
+	done
+}
+
+echo "按下任意键继续，如需退出，请按Ctrl C"
+read key
+install_homebrew
+install_cask_app
+install_cli_app
